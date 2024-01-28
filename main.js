d773837f7d92579f45793dcdd1e16a8bd7d2b9e9
Adam Abeshouse <adamabeshouse@Adams-MacBook-Air.local>
2024-01-28 15:04:51 -0500

    Submitting message works

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ function makeBubble(text, className) {
     return elt;
 }
 for (let i=0; i<20; i++) {
-    document.getElementsByClassName('container')[0].appendChild(makeBubble("test", "message-you"));
-    document.getElementsByClassName('container')[0].appendChild(makeBubble("test", "message-them"));
+    document.getElementsByClassName('messages-container')[0].appendChild(makeBubble("test", "message-you"));
+    document.getElementsByClassName('messages-container')[0].appendChild(makeBubble("test", "message-them"));
+}
+
+function addMessage() {
+    const text = document.getElementById("input").value;
+    if (!text.length) return;
+    document.getElementsByClassName('messages-container')[0].appendChild(makeBubble(text, "message-you"));
+    document.getElementById("input").value = "";
+    setTimeout(()=>{
+        document.getElementsByClassName('messages-container')[0].scrollTop = 
+        document.getElementsByClassName('messages-container')[0].scrollHeight;
+    });
 }
